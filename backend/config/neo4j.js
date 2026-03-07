1a006dd453103a387d74eb2e5fe0366abe768681
katherinexzhao <katherinexzhao2@gmail.com>
2026-03-07 03:07:54 -0500

    debug env

--- backend/config/neo4j.js
+++ backend/config/neo4j.js
@@ -1,6 +1,9 @@
 const neo4j = require('neo4j-driver');
 require('dotenv').config();
 
+console.log("NEO4J_URI:", process.env.NEO4J_URI);
+console.log("NEO4J_USER:", process.env.NEO4J_USER);
+
 const driver = neo4j.driver(
   process.env.NEO4J_URI,
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
